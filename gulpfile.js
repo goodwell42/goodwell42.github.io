2de958d4a45095d51dbc188f559379212a82df6d
goodwell42 <goodwell1998@gmail.com>
2018-12-17 01:43:40 +0800

    add commits && share && music-player && changed-icon || change subtitle.

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,20 @@ var uglify = require('gulp-uglify');
 var htmlmin = require('gulp-htmlmin');
 var htmlclean = require('gulp-htmlclean');
 
+// 增加变量定义gutil
+var gutil = require('gulp-util');
+
+gulp.task('uglify', function() {
+    return gulp.src(["www/dist_js/*.js"])
+      .pipe(concat('all.min.js'))
+      .pipe(uglify({outSourceMap: false, cache: true}))
+      .on('error', function (err) {
+        gutil.log(gutil.colors.red('[Error]'), err.toString());
+        throw err;
+      })
+      .pipe(gulp.dest("asserts/js"));
+});
+
 gulp.task('minify-css', function() {
     return gulp.src('./public/**/*.css')
         .pipe(minifycss())
@@ -22,11 +36,23 @@ gulp.task('minify-html', function() {
     .pipe(gulp.dest('./public'))
 });
 
+// 压缩public目录下的所有js
 gulp.task('minify-js', function() {
     return gulp.src('./public/**/*.js')
-        .pipe(uglify())
-        .pipe(gulp.dest('./public'));
+      .pipe(uglify())
+	  .on('error', function (err) { gutil.log(gutil.colors.red('[Error]'), err.toString()); }) //增加这一行
+      .pipe(gulp.dest('./public'));
+		
 });
+
+gulp.task('minify-js', function() {
+    return gulp.src('./public/dist/music.js')
+      .pipe(uglify())
+	  .on('error', function (err) { gutil.log(gutil.colors.red('[Error]'), err.toString()); }) //增加这一行
+      .pipe(gulp.dest('./public/dist'));
+		
+});
+
 
 gulp.task('default', [
     'minify-html','minify-css','minify-js'
