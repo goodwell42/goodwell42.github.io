b87ececb99901766d6734647ac62612c36fba907
goodwell42 <goodwell1998@gmail.com>
2018-12-18 00:01:22 +0800

    add sitemap && music cover

--- public/dist/music.js
+++ public/dist/music.js
@@ -7,25 +7,25 @@ const ap = new APlayer({
         name: 'You Are Beautiful',
         artist: 'James Blunt',
         url: 'http://www.ytmp3.cn/down/53969.mp3',
-        cover: 'http://www.yinyuetai.com/fanclub/photo-detail/8896/154821',
+        cover: 'http://pjvbe4vjy.bkt.clouddn.com/james.jpg',
       },
       {
         name: "Let Her Go",
         artist: 'Passenger',
         url: 'http://www.ytmp3.cn/down/49652.mp3',
-        cover: ' ',
+        cover: 'http://pjvbe4vjy.bkt.clouddn.com/passenger.jpg',
       },
       {
         name: 'Without You',
         artist: 'Avicii',
         url: 'http://www.ytmp3.cn/down/51095.mp3',
-        cover: 'http://oeff2vktt.bkt.clouddn.com/image/96.jpg',
+        cover: 'http://pjvbe4vjy.bkt.clouddn.com/avicii.png',
       },
       {
         name: '风筝误',
         artist: '刘珂矣',
         url: 'http://up.mcyt.net/?down/46644.mp3',
-        cover: 'http://oeff2vktt.bkt.clouddn.com/image/96.jpg',
+        cover: 'http://pjvbe4vjy.bkt.clouddn.com/%E9%A3%8E%E7%AD%9D%E8%AF%AF.jpg',
       }
     ]
 });
